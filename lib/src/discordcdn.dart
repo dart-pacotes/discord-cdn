b8e134d36dd60287b9f174c8876b3fec77a28087
freitzzz <1160907@isep.ipp.pt>
2022-05-16 19:24:16 +0100

    include doc for withBotToken function

--- lib/src/discordcdn.dart
+++ lib/src/discordcdn.dart
@@ -1,6 +1,10 @@
 import 'package:discordcdn/discordcdn.dart';
 import 'package:http/http.dart' as http;
 
+///
+/// Returns an instance of [BotDiscordClient] which
+/// takes on a token that authorizes the bot for requests.
+///
 DiscordClient withBotToken(final String token) {
   return BotDiscordClient(
     token: token,
